4c672d684c13177635d9939f0a0c70c21a150709
Viktor Karpov <viktor.s.karpov@gmail.com>
2014-05-25 14:17:24 +0400

    Структурирует тесты для /:id

--- tests/spec/http-lever-id.js
+++ tests/spec/http-lever-id.js
@@ -6,129 +6,121 @@ var data = require('../mocks/data-test.json');
 var jade = require('jade');
 
 describe('Ручка /:id', function() {
-	it('правильный HTML в ответ на обычный запрос', function(done) {
-		var testId = 1;
 
-		request(app)
-			.get('/' + testId)
-			.expect('Content-Type', 'text/html; charset=utf-8')
-			.expect(200)
-			.end(function(err, res) {
-				if (err) throw err;
+	describe('в зависимости от заголовка X-Requested-With', function() {
+		it('отдает HTML', function(done) {
+			request(app)
+				.get('/1')
+				.expect(200)
+				.end(function(err, res) {
+					res.should.be.html;
+					done();
+				});
+		});
+		it('отдает JSON', function(done) {
+			request(app)
+				.get('/1')
+				.set('X-Requested-With', 'XMLHttpRequest')
+				.end(function(err, res) {
+					res.should.be.json;
+					done();
+				});
+		});
+	});
 
-				jade.renderFile(
-					'templates/index.jade', 
-					{
-						urls: {
-							prev: null,
-							next: '/2'
+	describe('если попросили данные о существующей благодарности', function(done) {
+		it('вернет определенную разметку страницы благодарности', function(done) {
+			request(app)
+				.get('/1')
+				.end(function(err, res) {
+					if (err) throw err;
+
+					jade.renderFile(
+						'templates/index.jade',
+						{
+							urls: {
+								prev: null,
+								next: '/2'
+							},
+							title: 'Я благодарю',
+							item: data[0]
 						},
-						title: 'Я благодарю',
-						item: data[testId - 1]
-					}, 
-					function(err, html) {
-						if (err) throw err;
-						res.text.should.equal(html);	
-						done();
+						function(err, html) {
+							if (err) throw err;
+							res.text.should.equal(html);
+							done();
+						}
+					);
+				});
+		});
+		it('вернет JSON определенной структуры, если это ajax-запрос', function(done) {
+			request(app)
+				.get('/1')
+				.set('X-Requested-With', 'XMLHttpRequest')
+				.end(function(err, res) {
+					res.body.should.have.properties('status', 'code', 'data');
+					res.body.data.item.should.have.properties('id', 'from', 'to', 'reason');
+
+					res.body.code.should.equal(200);
+					res.body.status.should.equal('success');
+
+					res.body.data.item.id.should
+						.be.a.Number
+						.and.be.above(0);
+
+					res.body.data.item.reason.should
+						.be.a.String
+						.and.be.not.empty;
+
+					res.body.data.item.from.should.have.properties('sex', 'name', 'nameGenetive', 'idVk');
+					res.body.data.item.to.should.have.properties('sex', 'name', 'nameGenetive', 'idVk');
+
+					var users = ['from', 'to'];
+					for (var i = 0, max = users.length; i < max; i++) {
+						with (res.body.data.item[users[i]]) {
+							sex.should
+								.be.a.String
+								.and.have.length(1);
+
+							name.should
+								.be.a.String
+								.and.be.not.empty;
+
+							nameGenetive.should
+								.be.a.String
+								.and.be.not.empty;
+
+							idVk.should
+								.be.a.Number
+								.and.be.above(0);
+						}
 					}
-				);
-			});
+
+					done();
+				});
+		});
 	});
 
-	it('JSON в ответ на X-Requested-With: XMLHttpRequest', function(done) {
-		request(app)
-			.get('/1')
-			.set('X-Requested-With', 'XMLHttpRequest')
-			.expect('Content-Type', 'application/json; charset=utf-8')
-			.expect(200, done);
-	});
+	describe('если попросили несуществующую благодарность', function() {
+		it('вернет 404, если это обычный запрос', function(done) {
+			request(app)
+				.get('/6')
+				.expect(404, done);
+		});
+		it('вернет JSON определенной структуры, если это ajax-запрос', function(done) {
+			request(app)
+				.get('/6')
+				.set('X-Requested-With', 'XMLHttpRequest')
+				.end(function(err, res) {
+					res.body.should.have.properties('code', 'status', 'message');
+					res.body.code.should.equal(404);
+					res.body.status.should.equal('error');
+					res.body.message
+						.should.be.a.String
+						.and.be.not.empty;
 
-	it('JSON: правильная структура и типы значений в благодарности', function(done) {
-		request(app)
-			.get('/1')
-			.set('x-requested-with', 'XMLHttpRequest')
-			.expect(200)
-			.end(function(err, res) {
-				res.body.should.have.properties('status', 'item');
-				res.body.item.should.have.properties('id', 'from', 'to', 'reason');
-				
-				res.body.item.id.should
-					.be.a.Number
-					.and.be.above(0);
-
-				res.body.item.reason.should
-					.be.a.String
-					.and.be.not.empty;
-
-				res.body.item.from.should.have.properties('sex', 'name', 'nameGenetive', 'idVk');
-				res.body.item.to.should.have.properties('sex', 'name', 'nameGenetive', 'idVk');
-
-				var users = ['from', 'to'];
-				for (var i = 0, max = users.length; i < max; i++) {
-					with (res.body.item[users[i]]) {
-						sex.should
-							.be.a.String
-							.and.have.length(1);
-
-						name.should
-							.be.a.String
-							.and.be.not.empty;
-
-						nameGenetive.should
-							.be.a.String
-							.and.be.not.empty;
-
-						idVk.should
-							.be.a.Number
-							.and.be.above(0);
-					}
-				}
-
-				done();
-			});
-	});
-
-	it('JSON: ошибка, когда просим несуществуюший ID', function(done) {
-		request(app)
-			.get('/-1') // такого ID не может быть, само собой
-			.set('x-requested-with', 'XMLHttpRequest')
-			// нужен ли другой статус-код в случае ошибки? как его будет обрабатывать backbone?
-			.expect(200)
-			.end(
-				isErrorReturned(done)
-			);
-	});
-
-	it('JSON: ошибка, когда просим слишком большой ID', function(done) {
-		request(app)
-			.get('/99999999999')
-			.set('x-requested-with', 'XMLHttpRequest')
-			.expect(200)
-			.end(
-				isErrorReturned(done)
-			);
-	});
-
-	it('JSON: отдаёт ошибку, когда просим белиберду вместо ID', function(done) {
-		request(app)
-			// TODO как бы сюда разной белиберды напихать?
-			.get('/a_230dhZ":S*@#@K!J#HKJSHADJH)(AS*D')
-			.set('x-requested-with', 'XMLHttpRequest')
-			.expect(200)
-			.end(
-				isErrorReturned(done)
-			);
+					done();
+				});
+		});
 	});
 });
-
-function isErrorReturned(done) {
-	return function(err, res) {
-		res.body.should.have.properties('status', 'message');
-
-		res.body.status.should.be.false;
-		res.body.message
-			.should.be.a.String
-			.and.be.not.empty;
-		done();
-	}
-}
