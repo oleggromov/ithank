908bf3e7d5382b5b6783399cda67dce74e2e9219
Gromych <gromych@yandex-team.ru>
2014-11-04 18:26:45 +0300

    Статус 302, чтобы редиректы не кэшировались

--- server/controllers/redirect.js
+++ server/controllers/redirect.js
@@ -3,7 +3,10 @@ var collectionThank = require('models/thank');
 module.exports = function(req, res) {
 	collectionThank.findOne({}).sort('-id').exec()
 		.then(function(item) {
+			res.statusCode = 302;
+
 			if (!item) {
+				res.statusCode = 404;
 				res.redirect('/err/404');
 			}
 
